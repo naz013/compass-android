2df70f8a41507346e40eb3cebff3b34fcbd4e8c7
Nazar Suhovich <n.suhovich@gmail.com>
2019-05-01 18:28:47 +0300

    Add new simple compass view.

--- app/src/main/java/com/github/naz013/compassapp/view/SimpleOneCompassView.kt
+++ app/src/main/java/com/github/naz013/compassapp/view/SimpleOneCompassView.kt
@@ -8,14 +8,22 @@ import android.graphics.PointF
 import android.util.AttributeSet
 import androidx.annotation.ColorInt
 import com.github.naz013.compassapp.theming.Palette
+import com.github.naz013.compassapp.utils.ViewUtils
 import timber.log.Timber
+import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
 
 class SimpleOneCompassView : BaseCompassView {
 
     private val paint = Paint()
+    private var angleMap: MutableMap<Float, AngledLine> = mutableMapOf()
 
     private var mWidth: Int = 0
     private var mHeight: Int = 0
+    private var mShortLineLength: Float = 20f
+    private var mLongLineLength: Float = 50f
+    private var mNorthRadius: Float = 10f
     private var cX: Int = 0
     private var cY: Int = 0
     private val degreesCorrection = -90f
@@ -25,15 +33,18 @@ class SimpleOneCompassView : BaseCompassView {
     private var northColor = northColor()
     private var currentColor = currentColor()
 
-    constructor(context: Context): this(context, null)
+    constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        mShortLineLength = ViewUtils.dp2px(context, 15).toFloat()
+        mLongLineLength = ViewUtils.dp2px(context, 40).toFloat()
+        mNorthRadius = mShortLineLength / 2f
+
         paint.style = Paint.Style.FILL
         paint.color = Color.WHITE
-
-
+        paint.strokeWidth = ViewUtils.dp2px(context, 2).toFloat()
     }
 
     override fun updateColors(palette: Palette) {
@@ -49,11 +60,28 @@ class SimpleOneCompassView : BaseCompassView {
             super.onDraw(canvas)
         } else {
             canvas.drawColor(bgColor)
-            canvas.save()
+
             val rotationAngle = degreesCorrection - degrees
+
+            canvas.save()
             canvas.rotate(rotationAngle, cX.toFloat(), cY.toFloat())
 
+            angleMap.values.forEach { al ->
+                if (al.isAnchor) {
+                    paint.color = northColor
+                    val cx = (al.start.x + al.end.x) / 2f
+                    val cy = (al.start.y + al.end.y) / 2f
+                    canvas.drawCircle(cx, cy, mNorthRadius, paint)
+                } else {
+                    paint.color = lineColor
+                    canvas.drawLine(al.start.x, al.start.y, al.end.x, al.end.y, paint)
+                }
+            }
+
             canvas.restore()
+
+            paint.color = currentColor
+            canvas.drawLine(cX.toFloat(), 0f, cX.toFloat(), mLongLineLength * 2f, paint)
             super.onDraw(canvas)
         }
     }
@@ -66,11 +94,11 @@ class SimpleOneCompassView : BaseCompassView {
         cX = mWidth / 2
         cY = mHeight / 2
 
-        val minRadius = mWidth.toFloat() * 0.4f / 2f
+        val minRadius = mWidth.toFloat() / 2f
         Timber.d("onMeasure: radius -> $minRadius, ")
 
         setupText(cX, cY, minRadius.toInt())
-        calculateLines(minRadius, 0f)
+        calculateLines(minRadius)
     }
 
     @ColorInt
@@ -86,11 +114,47 @@ class SimpleOneCompassView : BaseCompassView {
     private fun dotColor(): Int = palette?.colorOnPrimary ?: Color.WHITE
 
 
-    private fun calculateLines(minRadius: Float, step: Float) {
-
+    private fun calculateLines(minRadius: Float) {
+        angleMap.clear()
+        var angle = 0f
+        while (angle < 360f) {
+            angleMap[angle] = lineCoordinates(angle, minRadius)
+            angle += 30f
+        }
+        Timber.d("calculateLines: $angleMap")
     }
 
-    data class AngledLine(var ring: Int = 0, var angle: Float = 0.0f, var point: PointF = PointF(0.0f, 0.0f), var isAnchor: Boolean = false)
+    private fun lineCoordinates(angle: Float, radius: Float): AngledLine {
+        val x1 = cX.toFloat() + radius * cos(Math.toRadians(angle.toDouble()))
+        val y1 = cY.toFloat() + radius * sin(Math.toRadians(angle.toDouble()))
+
+        val roundAngle = try {
+            String.format(Locale.US, "%.2f", angle).toFloat()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            0.0f
+        }
+        var length = if (roundAngle.toInt() % 90 == 0) {
+            mLongLineLength
+        } else {
+            mShortLineLength
+        }
+        if (roundAngle == 0f) {
+            length = mShortLineLength
+        }
+        val x2 = cX.toFloat() + (radius - length) * cos(Math.toRadians(angle.toDouble()))
+        val y2 = cY.toFloat() + (radius - length) * sin(Math.toRadians(angle.toDouble()))
+        return AngledLine(
+            angle,
+            PointF(x1.toFloat(), y1.toFloat()),
+            PointF(x2.toFloat(), y2.toFloat()),
+            roundAngle == 0f
+        )
+    }
+
+    data class AngledLine(var angle: Float, var start: PointF = PointF(0.0f, 0.0f),
+                          var end: PointF = PointF(0.0f, 0.0f), var isAnchor: Boolean = false
+    )
 
     companion object {
     }
